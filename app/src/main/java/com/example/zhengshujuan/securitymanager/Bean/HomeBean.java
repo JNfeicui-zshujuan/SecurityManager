07cf6341cb935ce355919b2a8d7fc075f66183cf
JNfeicui-zshujuan <1690486348@qq.com>
2016-05-12 10:13:45 +0800

    软件卸载

---
+++ app/src/main/java/com/example/zhengshujuan/securitymanager/Bean/HomeBean.java
@@ -0,0 +1,11 @@
+package com.example.zhengshujuan.securitymanager.Bean;
+
+/**
+ * Created by zhengshujuan on 2016/5/10.
+ */
+public class HomeBean {
+    public String decs;
+    public int pic;
+
+
+}
